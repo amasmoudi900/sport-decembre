5ac4fbd8c9f5a7e27fe2d1f81c1aecb5ab919c96
Abderrahmen Masmoudi <amasmoudi90@gmail.com>
2020-12-20 09:57:31 +0100

    Fixing app.component.html by adding blob,videos,word-cup, result components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,11 @@ import { FooterComponent } from './components/footer/footer.component';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { NewsComponent } from './components/news/news.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { VideosComponent } from './components/videos/videos.component';
+import { NextMatchComponent } from './components/next-match/next-match.component';
+import { ResultComponent } from './components/result/result.component';
+import { WordCupComponent } from './components/word-cup/word-cup.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +21,12 @@ import { NewsComponent } from './components/news/news.component';
     FooterComponent,
     LoginComponent,
     SignupComponent,
-    NewsComponent
+    NewsComponent,
+    BlogComponent,
+    VideosComponent,
+    NextMatchComponent,
+    ResultComponent,
+    WordCupComponent
   ],
   imports: [
     BrowserModule,
